49e8d55535ebe7f7806b0b09f31e3869476c729e
Yu <sunny7360@naver.com>
2025-03-30 19:02:16 +0900

    2557 Hello World!

---
+++ Bronze/Print_Operation/2557.cs
@@ -0,0 +1,13 @@
+using System; // 기본 using 정의 
+using static System.Console; // 정의를 미리 해놓음으로서 Console을 앞에 안 써도됨
+
+namespace Hello_World
+{
+    class MainAPP
+    {
+        static void Main() //메인문
+        {
+            WriteLine("Hello World!"); // Console 생략함
+        }
+    }
+}
