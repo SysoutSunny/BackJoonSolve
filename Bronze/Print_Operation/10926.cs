728156a5cebd67bb6240f91fc65f082647a186bb
Yu <sunny7360@naver.com>
2025-04-07 16:57:03 +0900

    10926 ??!

---
+++ Bronze/Print_Operation/10926.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+namespace BackJoon
+{
+    class _10926
+    {
+        static void Main()
+        {
+            string s = ReadLine();
+            WriteLine(s + "??!"); 
+            // 한 줄로 쓸 수 있음 (WriteLine(ReadLine() + "??!");)
+        }
+    }
+}
