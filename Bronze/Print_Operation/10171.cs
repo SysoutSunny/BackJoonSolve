592c8c13ddae8e5bdf17b7f165c8143650415078
Yu <sunny7360@naver.com>
2025-04-09 18:09:34 +0900

    10171 고양이 출력

---
+++ Bronze/Print_Operation/10171.cs
@@ -0,0 +1,19 @@
+using System;
+using static System.Console;
+
+namespace BackJoon
+{
+    // 고양이 출력하기
+    class _10171
+    {
+        static void Main()
+        {
+            // C#에서는 역슬래시 출력은 \\로 해야 하나로 보인다
+            // 역슬래시는 여러가지 기능을 해주기 때문에 하나만 쓰면 안된다. (ex \n 줄바꿈 같은것)
+            WriteLine("\\    /\\");
+            WriteLine(" )  ( ')");
+            WriteLine("(  /  )");
+            WriteLine(" \\(__)|");
+        }
+    }
+}
