3bed78378ef4c16f68bcf2807af9b4c7555d937d
Yu <sunny7360@naver.com>
2025-04-09 18:22:11 +0900

    10172 개 출력하기

---
+++ Bronze/Print_Operation/10172.cs
@@ -0,0 +1,20 @@
+using System;
+using static System.Console;
+
+namespace BackJoon
+{
+    // 개 출력하기
+    class _10172
+    {
+        static void Main()
+        {
+            // 문자열 내 특수 문자를 포함시키기 위해 역슬래시를 넣는 것을 이스케이프 시퀸스라고 한다 
+            // 공백을 주의하자 공백 때문에 계속 틀렸다.
+            WriteLine("|\\_/|");
+            WriteLine("|q p|   /}");
+            WriteLine("( 0 )\"\"\"\\");
+            WriteLine("|\"^\"`    |");
+            WriteLine("||_/=\\\\__|");
+        }
+    }
+}
