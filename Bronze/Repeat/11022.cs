bfcb02e1f562e5d6a7a72c978b80e2b08a616a2e
Yu <sunny7360@naver.com>
2025-04-25 16:46:33 +0900

    11022 A+B -8

---
+++ Bronze/Repeat/11022.cs
@@ -0,0 +1,32 @@
+using System;
+using static System.Console;
+
+namespace BackJoon
+{
+    // A+B-8
+    // 더욱 세련되게 푸는 문제
+    class _11022
+    {
+        static void Main()
+        {
+            int t = int.Parse(ReadLine()); // 테스트 케이스 갯수를 몇개 입력 하는 변수
+            int[] a = new int[t]; // A+B의 A의 배열
+            int[] b = new int[t]; // A+B의 B의 배열
+            int[] sum = new int[t]; // A+B의 합 배열
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = ReadLine().Split();
+
+                a[i] = int.Parse(s[0]); // a배열에 저장
+                b[i] = int.Parse(s[1]); // b배열에 저장
+                sum[i] = a[i] + b[i]; // A+B를 sum 배열에 저장
+            }
+            // 출력문
+            for (int i = 0; i < t; i++)
+            {
+                WriteLine($"Case #{i + 1}: " + a[i] + " + " + b[i] + " = " + sum[i]);
+            }
+        }
+    }
+}
